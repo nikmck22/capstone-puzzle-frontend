b4be19dde9cdd0d48bb441160b34c05f24a0fccb
Nikki <nikmck22@gmail.com>
2020-05-06 20:32:03 -0400

    add logout page

---
+++ src/views/Logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="logout">
+    <p>You have now been logged out</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  created: function() {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    this.$router.push("/");
+  }
+};
+
+</script>
