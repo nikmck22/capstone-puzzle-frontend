6228ef23dbb531531e79becf2c8994da879c2853
Nikki <nikmck22@gmail.com>
2020-05-10 16:24:17 -0400

    theme nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,123 @@
 <template>
   <div id="app">
-   			<!-- Header -->
-				<div id="header-wrapper">
-					<header id="header" class="container">
+    <div id="page-wrapper">
 
-						<!-- Logo -->
-							<div id="logo">
-								<h1><a href="index.html">Verti</a></h1>
-							</div>
+    <!-- Header -->
+    <div id="header-wrapper">
+      <header id="header" class="container">
 
-						<!-- Nav -->
-							<nav id="nav">
-								<ul>
-									<li class="current"><a href="index.html">Welcome</a></li>
-									<li>
-										<a href="#">Dropdown</a>
-										<ul>
-											<li><a href="#">Lorem ipsum dolor</a></li>
-											<li><a href="#">Magna phasellus</a></li>
-											<li>
-												<a href="#">Phasellus consequat</a>
-												<ul>
-													<li><a href="#">Lorem ipsum dolor</a></li>
-													<li><a href="#">Phasellus consequat</a></li>
-													<li><a href="#">Magna phasellus</a></li>
-													<li><a href="#">Etiam dolore nisl</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Veroeros feugiat</a></li>
-										</ul>
-									</li>
-									<li><a href="/left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="/right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="/no-sidebar.html">No Sidebar</a></li>
-								</ul>
-							</nav>
+        <!-- Nav -->
+          <nav id="nav">
+            <ul>
+              <li class="current"><a href="/">Welcome</a></li>
+              <li>
+                <a href="#">Menu</a>
+                <ul>
+                  <li><a href="/login">Login</a></li>
+                  <li><a href="/logout">Logout</a></li>
+                  <li>
+                    <a href="#">Phasellus consequat</a>
+                    <ul>
+                      <li><a href="#">Lorem ipsum dolor</a></li>
+                      <li><a href="#">Phasellus consequat</a></li>
+                      <li><a href="#">Magna phasellus</a></li>
+                      <li><a href="#">Etiam dolore nisl</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Veroeros feugiat</a></li>
+                </ul>
+              </li>
+              <!-- <li><a href="/left-sidebar.html">Left Sidebar</a></li>
+              <li><a href="/right-sidebar.html">Right Sidebar</a></li>
+              <li><a href="/no-sidebar.html">No Sidebar</a></li> -->
+            </ul>
+          </nav>
 
-					</header>
-				</div>
-    <router-view/>
-    			<!-- Footer -->
-				<div id="footer-wrapper">
-					<footer id="footer" class="container">
-						<div class="row">
-							<div class="col-3 col-6-medium col-12-small">
+      </header>
+    </div>
 
-								<!-- Links -->
-									<section class="widget links">
-										<h3>Random Stuff</h3>
-										<ul class="style2">
-											<li><a href="#">Etiam feugiat condimentum</a></li>
-											<li><a href="#">Aliquam imperdiet suscipit odio</a></li>
-											<li><a href="#">Sed porttitor cras in erat nec</a></li>
-											<li><a href="#">Felis varius pellentesque potenti</a></li>
-											<li><a href="#">Nullam scelerisque blandit leo</a></li>
-										</ul>
-									</section>
+      <router-view/>
 
-							</div>
-							<div class="col-3 col-6-medium col-12-small">
+    <div id="footer-wrapper">
+      <footer id="footer" class="container">
+        <div class="row">
+          <div class="col-3 col-6-medium col-12-small">
 
-								<!-- Links -->
-									<section class="widget links">
-										<h3>Random Stuff</h3>
-										<ul class="style2">
-											<li><a href="#">Etiam feugiat condimentum</a></li>
-											<li><a href="#">Aliquam imperdiet suscipit odio</a></li>
-											<li><a href="#">Sed porttitor cras in erat nec</a></li>
-											<li><a href="#">Felis varius pellentesque potenti</a></li>
-											<li><a href="#">Nullam scelerisque blandit leo</a></li>
-										</ul>
-									</section>
+            <!-- Links -->
+              <section class="widget links">
+                <h3>Random Stuff</h3>
+                <ul class="style2">
+                  <li><a href="#">Etiam feugiat condimentum</a></li>
+                  <li><a href="#">Aliquam imperdiet suscipit odio</a></li>
+                  <!-- <li><a href="#">Sed porttitor cras in erat nec</a></li>
+                  <li><a href="#">Felis varius pellentesque potenti</a></li>
+                  <li><a href="#">Nullam scelerisque blandit leo</a></li> -->
+                </ul>
+              </section>
 
-							</div>
-							<div class="col-3 col-6-medium col-12-small">
+          </div>
+          <div class="col-3 col-6-medium col-12-small">
 
-								<!-- Links -->
-									<section class="widget links">
-										<h3>Random Stuff</h3>
-										<ul class="style2">
-											<li><a href="#">Etiam feugiat condimentum</a></li>
-											<li><a href="#">Aliquam imperdiet suscipit odio</a></li>
-											<li><a href="#">Sed porttitor cras in erat nec</a></li>
-											<li><a href="#">Felis varius pellentesque potenti</a></li>
-											<li><a href="#">Nullam scelerisque blandit leo</a></li>
-										</ul>
-									</section>
+            <!-- Links -->
+              <section class="widget links">
+                <h3>Random Stuff</h3>
+                <ul class="style2">
+                  <li><a href="#">Etiam feugiat condimentum</a></li>
+                  <li><a href="#">Aliquam imperdiet suscipit odio</a></li>
+                  <!-- <li><a href="#">Sed porttitor cras in erat nec</a></li>
+                  <li><a href="#">Felis varius pellentesque potenti</a></li>
+                  <li><a href="#">Nullam scelerisque blandit leo</a></li> -->
+                </ul>
+              </section>
 
-							</div>
-							<div class="col-3 col-6-medium col-12-small">
+          </div>
+          <div class="col-3 col-6-medium col-12-small">
 
-								<!-- Contact -->
-									<section class="widget contact last">
-										<h3>Contact Us</h3>
-										<ul>
-											<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-											<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-											<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-											<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-											<li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
-										</ul>
-										<p>1234 Fictional Road<br />
-										Nashville, TN 00000<br />
-										(800) 555-0000</p>
-									</section>
+            <!-- Links -->
+              <section class="widget links">
+                <h3>Random Stuff</h3>
+                <ul class="style2">
+                  <li><a href="#">Etiam feugiat condimentum</a></li>
+                  <li><a href="#">Aliquam imperdiet suscipit odio</a></li>
+                  <!-- <li><a href="#">Sed porttitor cras in erat nec</a></li>
+                  <li><a href="#">Felis varius pellentesque potenti</a></li>
+                  <li><a href="#">Nullam scelerisque blandit leo</a></li> -->
+                </ul>
+              </section>
 
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-12">
-								<div id="copyright">
-									<ul class="menu">
-										<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</footer>
-				</div>
+          </div>
+          <div class="col-3 col-6-medium col-12-small">
+
+            <!-- Contact -->
+              <section class="widget contact last">
+                <h3>Contact Us</h3>
+                <!-- <ul>
+                  <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                  <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                  <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                  <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+                  <li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
+                </ul> -->
+                <p>1234 Fictional Road<br />
+                Nashville, TN 00000<br />
+                (800) 555-0000</p>
+              </section>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div id="copyright">
+              <ul class="menu">
+                <li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+    </div>
 
   </div>
 </template>
